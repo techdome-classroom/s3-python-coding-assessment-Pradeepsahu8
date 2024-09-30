941457b08e70a63bdee0de4911e72f2fb3d6b246
Pradeepsahu8 <usesforsomething@gmail.com>
2024-09-30 16:06:55 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,7 +6,18 @@ class Solution(object):
         """
         pass
 
+ stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
 
+        for char in s:
+            if char in mapping:  # If it's a closing bracket
+                top_element = stack.pop() if stack else '#'  # Use '#' as a placeholder for an empty stack
+                if mapping[char] != top_element:  # Check if it matches the expected opening bracket
+                    return False
+            else:
+                stack.append(char)  # It's an opening bracket, push onto the stack
+
+        return not stack 
 
 
 
